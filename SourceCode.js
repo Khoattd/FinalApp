b06930824af37b33b7dafe19dbb01e4c462773fa
Khoattd <Khoattd@nustechnology.com>
2019-01-21 11:35:28 +0700

    addSourceCode.js
    Add sourceCode.JS
    Transfer script in HTML to external JS file

---
+++ SourceCode.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() {
+
+    $( "button" ).click(function( event ) {
+        $('#div3, #div2').toggleClass("Hide");
+
+    });
+
+/*
+function myFunction() {
+$('#div3, #div2').toggleClass("Hide");
+};*/
+});
