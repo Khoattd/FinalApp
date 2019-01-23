af5794ff881388d20ebb4b9e630d0819462b89ef
Khoattd <Khoattd@nustechnology.com>
2019-01-23 16:06:35 +0700

    testing Validation

--- SourceCode.js
+++ SourceCode.js
@@ -1,12 +1,18 @@
 $( document ).ready(function() {
 
-    $( "button" ).click(function( event ) {
+    $( "button" ).contextmenu(function( event ) {
         $('#div3, #div2').toggleClass("Hide");
 
     });
+ 
 
+$(window).on("load",(function() {
+    return alert("Bye now!");
+  }));
+ 
 /*
 function myFunction() {
 $('#div3, #div2').toggleClass("Hide");
 };*/
 });
+
