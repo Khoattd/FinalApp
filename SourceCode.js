ff08225ca7fa2fe562a49f6cd5472ecb41be052b
Khoattd <Khoattd@nustechnology.com>
2019-01-24 15:57:32 +0700

    transfering to bootstrap

--- SourceCode.js
+++ SourceCode.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
 
     $( "button" ).click(function( event ) {
-        $('#div3, #div2').toggleClass("Hide");
+        $('#div3, #div2, .front').toggleClass("Hide");
 
     });
  
